892f756ddc4c92a6968994c2f2f5a028ace5c6ae
504BassSlapper <504BassSlapper@gmail.com>
2023-12-25 15:20:24 +0100

    implement a web server using chi

--- main.go
+++ main.go
@@ -1,5 +1,38 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/joho/godotenv"
+)
+
 func main() {
+
+	godotenv.Load(".env")
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("port not found in the environement")
+	}
+
+	router := chi.NewRouter()
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + port,
+	}
+
+	fmt.Printf("Server is starting on port: %s \n", port)
+	err := srv.ListenAndServe()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(srv)
 
 }
